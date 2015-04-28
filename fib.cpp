b7e7aa34ed033ad3870d3c377deed66bfbb59d19
Yuzo <hyzgog@gmail.com>
2015-04-28 22:15:05 +0800

    revise c++ version

--- fib.cpp
+++ fib.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-#include <sys/time.h>
+#include <boost/timer.hpp>
 #include <boost/format.hpp>
 
 using namespace std;
@@ -9,30 +9,28 @@ using namespace boost;
 inline double fib(double n);
 
 int main(int argc, char **argv) {
-	if(argc != 2){
+	if (argc != 2){
 		cout << "Usage: ./fib <n>" << endl;
 		return EXIT_FAILURE;
 	}
 	int start = atoi(argv[1]);
-	struct timeval tpstart, tpend; 
-	double timeuse, result;
+	timer t;
+	double result;
 
-	for(int i = 1; i <= start; i++){
-		gettimeofday(&tpstart, NULL);
+	for (int i = 1; i <= start; i++){
+		t.restart();
 		result = fib(i);
-		gettimeofday(&tpend, NULL);
-		timeuse = 1000000 * (tpend.tv_sec - tpstart.tv_sec) + tpend.tv_usec - tpstart.tv_usec;
-		timeuse /= 1000000;
-		cout << format("Calculated Fib(%1%) = %2%, Time Used: %3%s") % i % result % timeuse << endl;
+		cout << format("Calculated Fib(%1%) = %2%, Time Used: %3%s") % i % result % t.elapsed() << endl;
 	}
 
 	return EXIT_SUCCESS;
 }
 
 inline double fib(double n) {
-	if(n >= 3){
-		return fib(n-1) + fib(n-2);
-	} else{
+	if (n >= 3){
+		return fib(n - 1) + fib(n - 2);
+	}
+	else{
 		return 1;
 	}
 }
